7f72b1f67afc389c33efa20084af2ff9acb7d91d
Tim Grein <tim@4greins.de>
2021-08-04 16:28:21 +0200

    Add server

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,45 @@
-var path = require('path')
+const path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const validator = require('validator');
+const dotenv = require('dotenv');
+const fetch = require("node-fetch");
+const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const app = express()
+dotenv.config();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
-
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+app.get('/', (req, res) => {
+    res.sendFile('dist/index.html')
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.post('/analyzeText', async (req, res) => {
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    if (validator.isURL(req.body.textToAnalyze)) {
+        reqType = 'url';
+    } else {
+        reqType = 'txt';
+    }
+
+    console.log('analyzing text...');
+    const response = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&${reqType}=${req.body.input}`);
+    console.log('...finished analyzing!');
+
+    try {
+        const data = await response.json();
+        res.send(data);
+    } catch (error) {
+        console.log("error", error);
+    }
+});
+
+app.listen(8081, () => {
+    console.log('NLP App listening on port 8081!');
 })
