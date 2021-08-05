7ab4c8105550529efc879e1afbe77b3969270b20
Tim Grein <tim@4greins.de>
2021-08-05 19:33:03 +0200

    Remove alert and log from handleSubmit.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,8 +3,6 @@ const txtToAnalyzeField = document.getElementById('text-to-analyze');
 export const handleSubmit = (event, textToAnalyzeField = txtToAnalyzeField) => {
     event.preventDefault();
     const textToAnalyze = textToAnalyzeField.value;
-    console.log("Text to analyze: " + textToAnalyze)
-    alert(textToAnalyze);
     if (!textToAnalyze || textToAnalyze === "") {
         alert("Please enter text or a valid url!")
     } else {
